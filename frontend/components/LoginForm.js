a8682fc36d452723886ddc54494a99b87f51a4ba
Daniel <daniel.378.dj@gmail.com>
2023-02-23 10:53:14 +0700

    test

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,13 +1,13 @@
 import { Container } from "react-bootstrap";
 
 const LoginForm = () => {
-    return(
-        <>
-            <Container className="py-5 w-75">
-                <h1>Login</h1>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container className="py-5 w-75">
+        <h1>Logins</h1>
+      </Container>
+    </>
+  );
+};
 
 export default LoginForm;
