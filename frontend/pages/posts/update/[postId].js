cfb3f0302a4b00d776f6f87ba7a24446763ddde6
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2023-02-23 16:37:10 +0700

    commit

--- frontend/pages/posts/update/[postId].js
+++ frontend/pages/posts/update/[postId].js
@@ -6,7 +6,7 @@ const UpdatePost = () => {
     return (
         <Layout>
             <Container className="px-0 py-5">
-                <h1 className="text-center fw-bold">Update Blog</h1>
+                <h1 className="text-center fw-bold">{post}</h1>
                 <div className="d-flex justify-content-center pt-4 pb-3">
                     <Image src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg" alt="title" className="w-25" />
                 </div>
@@ -38,7 +38,7 @@ const UpdatePost = () => {
                     <div className="d-flex justify-content-end">
                         <div className="d-flex justify-content-between w-25">
                             <Link href={`/`}>
-                                <Button className="btn btn-danger fw-bold">Cancel</Button>
+                                <Button className="btn text-white btn-danger fw-bold">Cancel</Button>
                             </Link>
                             <Link href={`/`}>
                                 <Button className="btn btn-primary fw-bold" type="submit">Update</Button>
@@ -52,3 +52,21 @@ const UpdatePost = () => {
 }
 
 export default UpdatePost;
+
+async function getPostById(id){
+    const post = await fetch(`http://localhost:3001/posts/${id}`)
+                    .then(res => res.json())
+                    .catch(error => console.log(error))
+    return post?.data
+}
+
+export async function getStaticProps(){
+    const post = await getPostById() ?? []
+
+    return {
+        props: {
+            post
+        },
+        revalidate: 30
+    }
+}
--- frontend/pages/profile/view/[userId].js
+++ frontend/pages/profile/view/[userId].js
@@ -1,14 +1,46 @@
 import Layout from "@/components/Layout";
 import { Container } from "react-bootstrap";
+import { useRouter } from "next/router";
 
 const Profile = () => {
+    const router = useRouter();
+    const user = router.query.userId;
+    const userData = getUserById(user)
     return (
         <Layout>
             <Container className="px-0 py-5">
+                <h1>Profile</h1>
+                <p>Name: {user.name}</p>
                 
             </Container>
         </Layout>
     )
 }
 
-export default Profile;
+export default Profile; 
+
+async function getUserById(id){
+    const user = await fetch(`http://localhost:3001/users/${id}`)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+    return user?.data
+}
+
+export async function getStaticPaths(){
+    return{
+        paths: [
+            {params: {userId: '1'}}
+        ],
+        fallback: false
+    }
+}
+
+export async function getStaticProps(){
+    const user = await getUserById() ?? []
+    return {
+        props: {
+            user
+        },
+        revalidate: 30
+    }
+}
