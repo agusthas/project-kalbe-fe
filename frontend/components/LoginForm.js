df25f3d5c9107f3da8844282c18c99e9030950ef
Daniel <daniel.378.dj@gmail.com>
2023-02-24 15:55:54 +0700

    tampilan login/regis

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -3,8 +3,54 @@ import { Container } from "react-bootstrap";
 const LoginForm = () => {
   return (
     <>
-      <Container className="py-5 w-75">
-        <h1>Sing In to Your Account</h1>
+      <h1 className="text-center mb-5">Sign In to Your Account</h1>
+      <Container className="rounded mb-5 py-5 w-50 d-flex justify-content-center shadow">
+        <form className="w-100 px-5">
+          <div className="mb-3">
+            <label className="mb-1">Email Address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter your email..."
+            />
+          </div>
+          <div className="mb-3">
+            <label className="mb-1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter your password..."
+            />
+          </div>
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label
+                className="custom-control-label mx-2"
+                htmlFor="customCheck1"
+              >
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div className="d-grid mb-3">
+            <button type="submit" className="btn btn-primary py-2">
+              Sign In
+            </button>
+          </div>
+          <div className="text-center">
+            <p>
+              Don't have an account yet?{" "}
+              <a className="forgot-password text-right" href="/register">
+                Register Here!
+              </a>
+            </p>
+          </div>
+        </form>
       </Container>
     </>
   );
