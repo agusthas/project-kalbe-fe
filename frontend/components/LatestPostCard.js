af8485abaf64d63badd2b9c8856ef8f64f6336dc
ervin-sungkono <ervinsung@gmail.com>
2023-02-25 17:18:58 +0700

    refactor code fix date formatting hydration issue

--- frontend/components/LatestPostCard.js
+++ frontend/components/LatestPostCard.js
@@ -1,8 +1,11 @@
 import Image from "next/image"
 import parser from "html-react-parser"
 import { formatDate } from "@/lib/date"
+import { useEffect, useState } from "react"
 
 export default function LatestPostCard({post}){
+    const [dateString, setDateString] = useState("")
+    useEffect(() => setDateString(formatDate(new Date(post.createdAt))),[])
     return(
         <div class="card h-100 d-flex flex-column justify-content-between" style={{border: 'none'}}>
             <div style={{width: '100%', aspectRatio: '16 / 9', position: 'relative'}}>
@@ -12,7 +15,7 @@ export default function LatestPostCard({post}){
                 <h5 class="card-title fw-bold flex-grow-1 mb-1">{post.title}</h5>
                 <div className="d-flex justify-content-between w-100">
                     <p className="card-text mb-2">by <span className="fw-semibold">{post.author.name}</span></p>
-                    <p className="card-text mb-2">{formatDate(new Date(post.createdAt))}</p>
+                    <p className="card-text mb-2">{dateString}</p>
                 </div>
                 <p class="card-text line-clamp-4 text-muted mb-2">{parser(post.description)}</p>
                 <p className="badge py-2 px-3 rounded-pill bg-dark text-white mb-2">{post.category.name}</p>
