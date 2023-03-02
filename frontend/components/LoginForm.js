8560d9a6ec5bdf6b0016d0cbadcbe2127d8013e8
DanielJonthn <daniel.378.dj@gmail.com>
2023-03-02 11:24:08 +0700

    temp commit

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,59 +1,83 @@
-import { Container } from "react-bootstrap";
+import React from "react"
+import { Container } from "react-bootstrap"
 
-const LoginForm = () => {
-  return (
-    <>
-      <h1 className="text-center my-5">Sign In to Your Account</h1>
-      <Container className="rounded mb-5 py-5 w-50 d-flex justify-content-center shadow">
-        <form className="w-100 px-5">
-          <div className="mb-4">
-            <label className="mb-1">Email Address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter your email..."
-            />
-          </div>
-          <div className="mb-4">
-            <label className="mb-1">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter your password..."
-            />
-          </div>
-          <div className="mb-4">
-            <div className="custom-control custom-checkbox">
+class LoginForm extends React.Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+  
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  
+  handleSubmit = event => {
+    const {email, password} = this.state
+    
+    event.preventDefault()
+    console.log()
+  }
+  
+  render(){
+    return (
+      <>
+        <h1 className="text-center my-5">Sign In to Your Account</h1>
+        <Container className="rounded mb-5 py-5 w-50 d-flex justify-content-center shadow">
+          <form action="" className="w-100 px-5" onSubmit={this.handleSubmit}>
+            <div className="mb-4">
+              <label className="mb-1">Email Address</label>
               <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Enter your email..."
+                onChange={this.handleChange}
               />
-              <label
-                className="custom-control-label mx-2"
-                htmlFor="customCheck1"
-              >
-                Remember me
-              </label>
             </div>
-          </div>
-          <div className="d-grid mb-4">
-            <button type="submit" className="btn btn-primary py-2">
-              Sign In
-            </button>
-          </div>
-          <div className="text-center">
-            <p>
-              Don't have an account yet?{" "}
-              <a className="forgot-password text-right" href="/register">
-                Register Here!
-              </a>
-            </p>
-          </div>
-        </form>
-      </Container>
-    </>
-  );
-};
+            <div className="mb-4">
+              <label className="mb-1">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Enter your password..."
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label
+                  className="custom-control-label mx-2"
+                  htmlFor="customCheck1"
+                >
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div className="d-grid mb-4">
+              <button type="submit" className="btn btn-primary py-2">
+                Sign In
+              </button>
+            </div>
+            <div className="text-center">
+              <p>
+                Don't have an account yet?{" "}
+                <a className="forgot-password text-right" href="/register">
+                  Register Here!
+                </a>
+              </p>
+            </div>
+          </form>
+        </Container>
+      </>
+    );
+  };
+}
 
 export default LoginForm;
--- frontend/components/RegisterForm.js
+++ frontend/components/RegisterForm.js
@@ -1,51 +1,125 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
+import { register } from "@/modules/auth/api";
 
 const RegisterForm = () => {
+  const router = useRouter()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirm, setConfirm] = useState('')
+  const [phone, setPhone] = useState('')
+  
+  const nameChangeHandler = event => {
+    setName(event.target.value)
+  }
+
+  const emailChangeHandler = event => {
+    setEmail(event.target.value)
+  }
+
+  const passwordChangeHandler = event => {
+    setPassword(event.target.value)
+  }
+
+  const confirmChangeHandler = event => {
+    setConfirm(event.target.value)
+  }
+
+  const phoneChangeHandler = event => {
+    setPhone(event.target.value)
+  }
+
+  const submitHandler = event => {
+    event.preventDefault()
+    // console.log({
+    //   name: name,
+    //     email: email,
+    //     password: password,
+    //     confirm: confirm,
+    //     phone: phone
+    // })
+    if(password != confirm) return
+
+    const newUser = {
+        name: name,
+        email: email,
+        password: password,
+        phone: phone
+    }
+
+
+    
+    setName('')
+    setEmail('')
+    setPassword('')
+    setConfirm('')
+    setPhone('')
+
+    router.push('/')
+  }
+
   return (
     <>
       <h1 className="text-center my-5">Create Your Account</h1>
       <Container className="rounded mb-5 py-5 w-50 d-flex justify-content-center shadow">
-        <form className="w-100 px-5">
+        <form onSubmit={submitHandler} className="w-100 px-5">
           <div className="mb-4">
             <label className="mb-1">Name</label>
             <input
+              id="name"
               type="name"
               className="form-control"
               placeholder="Enter your name..."
+              value={name}
+              onChange={nameChangeHandler}
             />
           </div>
           <div className="mb-4">
             <label className="mb-1">Email Address</label>
             <input
+              id="email"
               type="email"
               className="form-control"
               placeholder="Enter your email..."
+              value={email}
+              onChange={emailChangeHandler}
             />
           </div>
           <div className="d-flex justify-content-between">
             <div className="mb-4" style={{ width: "325px" }}>
               <label className="mb-1">Password</label>
               <input
+                id="password"
                 type="password"
                 className="form-control"
                 placeholder="Enter your password..."
+                value={password}
+                onChange={passwordChangeHandler}
               />
             </div>
             <div className="mb-4" style={{ width: "325px" }}>
               <label className="mb-1">Confirm Password</label>
               <input
+                id="confirm"
                 type="password"
                 className="form-control"
                 placeholder="Confirm your password..."
+                value={confirm}
+                onChange={confirmChangeHandler}
               />
             </div>
           </div>
           <div className="mb-4">
             <label className="mb-1">Phone Number</label>
             <input
+              id="phone"
               type="text"
               className="form-control"
               placeholder="Enter your phone number..."
+              value={phone}
+              onChange={phoneChangeHandler}
             />
           </div>
           <div className="d-grid mb-4">
