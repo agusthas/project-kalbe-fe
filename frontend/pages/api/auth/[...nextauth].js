491c7ab60d08a250e5f5615b81b1b6575ca43149
SusuMantan <suzumantan@gmail.com>
2023-03-02 14:46:56 +0700

    nextauth integration

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,36 +1,56 @@
-// //TEMP
+import { login } from "@/modules/auth/api";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-// import NextAuth from "next-auth/next";
-// // import { CredentialsProvider } from "next-auth/providers";
+export default NextAuth({
+  secret: "SUPER_SECRET_KEY",
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
 
-// export default NextAuth({
-//     providers: [
-//         CredentialsProvider({
-//         name: "Email",
-//         credentials: {
-//           username: {
-//             label: "Email Address",
-//             type: "text",
-//             placeholder: "Input your email...",
-//           },
-//           password: {
-//             label: "Password",
-//             type: "password",
-//             placeholder: "Input your password...",
-//           },
-//         },
-//         async authorize(credentials, req) {
-//           const user = { id: "1", name: "Daniel Jonathan", email: "danieljonathan@gmail.com" };
-  
-//           if (user) {
-//             return user;
-//           } else {
-//             return null;
-//           }
-//         },
-//       }),
-//     ],
-//     pages: {
-//       signIn: "/login",
-//     },
-//   });
+        try {
+          const { data } = await login({ email, password });
+
+          return {
+            id: data.user.id,
+            name: data.user.name,
+            email: data.user.email,
+            image: data.user.avatar,
+            accessToken: data.token,
+          };
+        } catch (err) {
+          console.log(err);
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+
+      return session;
+    },
+  },
+});
