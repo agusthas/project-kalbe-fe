b96b11cee7dd582ad6f17bda970710870b434042
SusuMantan <suzumantan@gmail.com>
2023-03-07 13:25:39 +0700

    add delete route

--- backend/src/posts/comments/comments.service.ts
+++ backend/src/posts/comments/comments.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCommentDto } from './dto';
 
@@ -27,5 +31,37 @@ export class CommentsService {
     });
 
     return comment;
+  }
+
+  async remove(postId: number, userId: number, id: number) {
+    const comment = await this.prisma.comment.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        author: true,
+        post: true,
+      },
+    });
+
+    if (!comment) {
+      throw new NotFoundException(`Comment with ID ${id} not found`);
+    }
+
+    if (comment.author.id !== userId) {
+      throw new UnauthorizedException(
+        `You don't have permission to delete this comment`,
+      );
+    }
+
+    if (comment.post.id !== postId) {
+      throw new NotFoundException(`Post with ID ${postId} not found`);
+    }
+
+    return this.prisma.comment.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
